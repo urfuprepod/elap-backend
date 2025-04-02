c3bb2e9ad045e85babf3e9dd29257e3fb94b9724
urfuprepod <zivstar@ya.ru>
2025-04-02 05:37:07 +0500

    fix

--- src/emails/emails.controller.ts
+++ src/emails/emails.controller.ts
@@ -11,7 +11,7 @@ export class EmailsController {
     console.log(join(process.cwd(), 'src/templates'));
     // return null
     return this.emailsService.sendWelcomeEmail(
-      'Mark.Zaharenko@ya.ru',
+      'i.v.zakharov@urfu.ru',
       'пароль',
     );
   }
