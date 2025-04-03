4c1128004a00e5beae3e0a5a7eca0b94e2a71b66
urfuprepod <zivstar@ya.ru>
2025-04-03 08:45:10 +0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NextFunction, Request, Response, static as static_ } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({ origin: '*', methods: 'GET, POST, PUT, DELETE', preflightContinue: false, optionsSuccessStatus: 204 });
+
+  app.use(function (request: Request, response: Response, next: NextFunction) {
+    response.setHeader('Access-Control-Allow-Origin', 'http://elap.site:5500');
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+    response.setHeader('Content-Disposition', 'attachment');
+    next();
+  });
+
   await app.listen(process.env.PORT ?? 5500);
 }
 bootstrap();
